2693aba1bebad4a05596857f13511caa29192de7
fredy4eg <fredy97@mail.ru>
2015-04-22 22:38:50 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -3,32 +3,12 @@
 #include <stdlib.h>
 #include <math.h>
 #include <vector>
+#include "point.h"
 
 using namespace std;
 
 
-class Point
-{
-	friend class Shell;
-	private:		
-		double x,y;
-	public:
-		Point(){};
 
-		Point(double j,double k): x(j), y(k){};
-		
-		void ScanPoint()
-		{
-			cin>>x>>y;
-		}
-		
-		void PrintPoint()
-		{
-			cout<<x<<" "<<y;
-		}
-		
-		
-};
 
 class Shell
 {
