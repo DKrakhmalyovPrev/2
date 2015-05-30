d58937fa202b4602a6d7321083e8b74d8eda08e7
fredy4eg <fredy97@mail.ru>
2015-05-30 21:22:58 +0300

    Create chart.cpp

---
+++ chart.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <math.h>
+#include <vector>
+#include <map>
+#include <string.h>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+class Chart
+{
+	private:
+	
+	map<string, map<int,int> > U_T; //Пользователь, песня, сколько раз слушал
+	
+	public:
+		
+	void VoteRecord(string username, int trackID)//Пользователь прослушал песню
+	{
+		if( U_T.find(username) == U_T.end() )
+			U_T[username][trackID] = 1;
+		else
+		if( U_T.find(username)->second.find(trackID)==U_T.find(username)->second.end()  )
+			U_T[username][trackID] = 1;
+		else
+			U_T[username][trackID]++;
+	}
+
+	vector<int> GetTop(string username, int topSize) //возвращает topSize самых популярных песен, прослушанных пользователем username 
+	{
+		vector<pair<int, int>> top;
+		
+			for(auto it=U_T[username].begin(); it!=U_T[username].end(); it++)
+				top.push_back(*it);
+		//for(int i=0; i<top.size(); i++)
+		//	cout<<top[i].first<<" песню прослушали раз:"<<top[i].second<<"\n";
+		struct 
+		{
+        		bool operator()(pair<int,int>& a, pair<int, int>& b)
+        		{   
+        		    return a.second > b.second;
+        		}   
+		} customLess;
+
+		std::sort(top.begin(), top.end(), customLess);
+		vector<int> a;
+		for(int i=0; i<topSize; i++)
+			a.push_back(top[i].first);
+		return(a);					
+	}
+
+	int GetHistorySize(string username)
+	{
+		int q=0;
+		for(auto it=U_T[username].begin(); it!=U_T[username].end(); it++)
+			q+=(*it).second;
+		return(q);
+	}
+
+	int GetDistinctTrackSize(string username) //возвращает число различных песен, прослушанных пользователем username
+	{
+		int q=0;
+		for(auto it=U_T[username].begin(); it!=U_T[username].end(); it++)
+			q++;
+		return(q);	
+	}
+
+};
+
+
+int main()
+{
+	Chart a;
+	a.VoteRecord("vasya", 1);
+	a.VoteRecord("vasya", 1);
+	a.VoteRecord("vasya", 1);
+	a.VoteRecord("vasya", 2);
+	a.VoteRecord("vasya", 2);
+	a.VoteRecord("vasya", 3);
+	a.VoteRecord("vasya", 3);
+	a.VoteRecord("vasya", 3);
+	a.VoteRecord("vasya", 3);
+	a.VoteRecord("vasyan", 228);
+	a.VoteRecord("vasyan", 30);
+	a.VoteRecord("vasyan", 30);
+	a.VoteRecord("vasyan", 30);
+	a.VoteRecord("vasyan", 41);
+	a.VoteRecord("vasyan", 228);
+	a.VoteRecord("vasyan", 41);
+	a.VoteRecord("vasyan", 30);
+	a.VoteRecord("vasyan", 25);
+	vector<int> b;
+	b=a.GetTop("vasyan", 2);
+	for(int i=0;i<b.size();i++)
+		cout<<b[i]<<" ";
+	cout<<a.GetDistinctTrackSize("vasyan");
+	return(0);
+}
